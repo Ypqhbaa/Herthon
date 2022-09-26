315accabb229c0e6b70ddc2641fdfc85232d5578
Ypqhbaa <113593284+Ypqhbaa@users.noreply.github.com>
2022-09-26 17:24:17 +0300

    Update يوتيوب.py

--- jepthon/plugins/يوتيوب.py
+++ jepthon/plugins/يوتيوب.py
@@ -367,7 +367,7 @@ async def kakashi(event):
         end = datetime.now()
         ms = (end - start).seconds
         await cat.edit(
-            f"꙳ ¦ تم تنزيل بواسطة  : @Repthon ",
+            f"꙳ ¦ تم تنزيل بواسطة  : @Herthon ",
             parse_mode="html",
         )
     await event.client.delete_messages(
